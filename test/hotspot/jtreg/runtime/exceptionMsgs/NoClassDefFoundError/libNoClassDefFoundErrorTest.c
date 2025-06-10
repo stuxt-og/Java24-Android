7ccb8320f06137d17f108db81e19fffa9f454333
stuxt-og <stuxtyt@gmail.com>
2025-06-10 13:33:28 +0300

    Fixed NoClassDefFoundError test 2

--- test/hotspot/jtreg/runtime/exceptionMsgs/NoClassDefFoundError/libNoClassDefFoundErrorTest.c
+++ test/hotspot/jtreg/runtime/exceptionMsgs/NoClassDefFoundError/libNoClassDefFoundErrorTest.c
@@ -26,7 +26,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if TARGET_ARCH_NBITS != 32
+#ifndef _LP32
 JNIEXPORT void JNICALL
 Java_NoClassDefFoundErrorTest_callDefineClass(JNIEnv *env, jclass klass, jstring className) {
     const char *c_name = (*env)->GetStringUTFChars(env, className, NULL);
@@ -70,7 +70,7 @@ Java_NoClassDefFoundErrorTest_tryCallDefineClass(JNIEnv *env, jclass klass) {
 
 JNIEXPORT jboolean JNICALL
 Java_NoClassDefFoundErrorTest_tryCallFindClass(JNIEnv *env, jclass klass) {
-#if TARGET_ARCH_NBITS != 32
+#ifndef _LP64
     char* c_name = giant_string();
     if (c_name != NULL) {
         jclass cls = (*env)->FindClass(env, c_name);
